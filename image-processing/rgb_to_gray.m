5b3e9f9c46407b6ffc740de40ad4acbfa21e35c0
Vipul Sharma <vipuls526@gmail.com>
2019-02-11 11:48:53 +0530

    Update rgb_to_gray.m

--- image-processing/rgb_to_gray.m
+++ image-processing/rgb_to_gray.m
@@ -1,16 +1,44 @@
 clear
 clc
 
-FILE_NAME = 'images/rock-house.jpg';
+FILE_NAME = 'images/processed/rock-house/new-house.jpg';
+rockHouse = imread(FILE_NAME);
 
-[rockHouse, houseMap] = imread(FILE_NAME);
-fileInfo = dir(FILE_NAME);
-fileSize = fileInfo(1).bytes;
-fileSizeMB = fileSize/1024;
+% Here our goal is to convert the colour image into gray image, and there
+% exists a direct function for it
+% rgb2gray()
+% But showing no signs of laziness we are gonna create our own function
 
-grayHouse = rgb2gray(rockHouse);
 [height, width, dim] = size(rockHouse);
 
-cropHouse = imcrop(rockHouse, [0, 0, 7419, 4910]);
-% imshow(grayHouse);
-% imshow(cropHouse);
+
+pixel_count = 0;
+val_count = 0;
+for h = 1:height
+    for w = 1:width
+        pixel_count = pixel_count + 1;
+        
+        rval = rockHouse(h,w,1);
+        gval = rockHouse(h,w,2);
+        bval = rockHouse(h,w,3);
+       
+        val_count = val_count + double((rval + gval + bval)/3);
+    end
+end
+
+threshold = val_count/pixel_count;
+
+new_image = zeros(height, width);
+for h = 1:height
+    for w = 1:width
+        rval = rockHouse(h,w,1);
+        gval = rockHouse(h,w,2);
+        bval = rockHouse(h,w,3);
+        
+        value = double((rval + gval + bval)/3);
+        new_image(h,w) = value/128;
+    end
+end
+imshow(new_image);
+NEW_FILE = 'images/processed/rock-house/gray-house.jpg';
+imwrite(new_image, NEW_FILE);
